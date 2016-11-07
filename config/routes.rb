f3afe0bb9a7ae733c1654c61948c7fc0a3c00dce
Tsuchikawa Toshiki <toshiki@Tsuchikawa-no-MacBook-Air.local>
2016-11-07 11:34:33 +0900

    route整備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  root "questions", controller: 'questions', action: 'show', id: '1'
   resources :results, only: [:index]
   resources :answer_lists, only: [:index]
   resources :questions, only: [:index, :show, :update]
