09a81b3cee12d06e1f30edba1c30a59e4a75317b
Tsuchikawa Toshiki <toshiki@Tsuchikawa-no-MacBook-Air.local>
2016-11-25 15:41:24 +0900

    “heroku”

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,12 @@
 class Question < ActiveRecord::Base
   has_many :choices, dependent: :destroy
 
+  def self.order_by_ids(ids)
+    order_by = ["case"]
+    ids.each_with_index.map do |id, index|
+      order_by << "WHEN id='#{id}' THEN #{index}"
+    end
+    order_by << "end"
+    order(order_by.join(" "))
+  end
 end
